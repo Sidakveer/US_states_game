d4e737cf19aca746917f25221a372dd25a8eebc8
Sidakveer <sidakveersingh8@gmail.com>
2022-04-29 13:01:57 -0400

    complete basic functionality

--- main.py
+++ main.py
@@ -1,20 +1,42 @@
 import turtle
-from turtle import Screen
+from turtle import Screen, Turtle
 import pandas
 #
 screen = Screen()
 screen.title("us_states_game")
+screen.setup(width=730, height=500)
 
 image = "blank_states_img.gif"
 screen.addshape(image)
 turtle.shape(image)
 
-answer_state = screen.textinput("Guess a State", prompt="What's another state's name")
+answer_state = screen.textinput("Guess a State", prompt="What's another state's name").lower()
 
 data = pandas.read_csv("50_states.csv")
-print(data)
+state_list = (data.state).tolist()
+print(state_list)
 
 
-print(data[data.x )
+def create_turtle(x, y, input):
+    new_turtle = Turtle(visible=False)
+    new_turtle.penup()
+    new_turtle.goto(x, y)
+    new_turtle.speed("fastest")
+    new_turtle.write(input, align="center", font=("Arial", 12, ""))
 
-# screen.mainloop()
+count = 0
+game_on = True
+while game_on:
+    for state in state_list:
+        if answer_state == state.lower():
+            count += 1
+            state_data = data[data.state == state]
+            x_cor = int(state_data.x)
+            y_cor = int(state_data.y)
+            create_turtle(x_cor, y_cor, state)
+
+    answer_state = turtle.textinput(f"{count}/50 States Correct", prompt="What's another state's name").lower()
+    if count == 50:
+        game_on = False
+
+screen.mainloop()
